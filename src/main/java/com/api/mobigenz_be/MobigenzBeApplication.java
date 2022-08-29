f859ed14a6756a616ed502a09d919bf6583daec4
hoanam2k2 <hoanam2k2.tb@gmail.com>
2022-08-29 21:54:02 +0700

    hello

---
+++ src/main/java/com/api/mobigenz_be/MobigenzBeApplication.java
@@ -0,0 +1,13 @@
+package com.api.mobigenz_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobigenzBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobigenzBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/mobigenz_be/MobigenzBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.mobigenz_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobigenzBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
