532ac6c64a8d33ea30025de384a194047153e6d3
Phus <vumanhphu05122002@gmail.com>
2022-11-05 22:00:31 +0700

    [RQ041] Api thêm manufacterer

--- src/main/java/com/api/mobigenz_be/services/ManufacturersService.java
+++ src/main/java/com/api/mobigenz_be/services/ManufacturersService.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface ManufacturersService {
     List<ManufacturerDto> getList();
     ManufacturerDto getManufacturerByProductLineId(Integer  ProductLineId);
+
+    ManufacturerDto saveManufacturer(ManufacturerDto manufacturerDto);
 }
