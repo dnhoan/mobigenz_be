f048ead32450a382a45ae667ed62a33ccbc6b7e5
hoanam2k2 <hoanam2k2.tb@gmail.com>
2022-11-17 00:51:09 +0700

    [RQ049] Customer Order

--- src/main/java/com/api/mobigenz_be/repositories/OrderRepository.java
+++ src/main/java/com/api/mobigenz_be/repositories/OrderRepository.java
@@ -2,6 +2,9 @@ package com.api.mobigenz_be.repositories;
 
 import com.api.mobigenz_be.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +14,10 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
 
     List<Order> getOrdersByCustomerId(Integer customer_id);
 
+    @Modifying(clearAutomatically = true)
+    @Query("update Order o set o.orderStatus = :newOrderStatus, o.cancelNote = :note where o.id = :order_id")
+    void updateOrderStatus(
+                    @Param("newOrderStatus") Integer newOrderStatus,
+                     @Param("note") String note,
+                     @Param("order_id") Integer order_id);
 }
