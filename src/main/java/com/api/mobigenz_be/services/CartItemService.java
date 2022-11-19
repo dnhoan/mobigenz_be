808d18fc0c59eafab60042633fee2b23936c0300
Phus <vumanhphu05122002@gmail.com>
2022-11-19 22:18:40 +0700

    [RQ047] API CRUD quản lý nhân viên - bổ sung

--- src/main/java/com/api/mobigenz_be/services/CartItemService.java
+++ src/main/java/com/api/mobigenz_be/services/CartItemService.java
@@ -5,6 +5,10 @@ import com.api.mobigenz_be.DTOs.CartItemDTO;
 import java.util.List;
 
 public interface CartItemService {
+	CartItemDTO createCartItem(CartItemDTO cartItemDTO);
+
+	CartItemDTO updateCartItem(CartItemDTO cartItemDTO);
+
 	CartItemDTO addCartItem(CartItemDTO cartItemDTO, Integer customerId);
 	CartItemDTO updateCartItem(CartItemDTO cartItemDTO, Integer cart_id);
 	void deleteCartItem(Integer id);
--- src/main/java/com/api/mobigenz_be/services/CartItemServiceImp.java
+++ src/main/java/com/api/mobigenz_be/services/CartItemServiceImp.java
@@ -1,8 +1,5 @@
 package com.api.mobigenz_be.services;
 
-import com.api.mobigenz_be.entities.Cart;
-import com.api.mobigenz_be.entities.Customer;
-import com.api.mobigenz_be.entities.ProductDetail;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.api.mobigenz_be.DTOs.CartItemDTO;
@@ -11,20 +8,17 @@ import com.api.mobigenz_be.entities.CartItem;
 import com.api.mobigenz_be.repositories.CartItemRepository;
 import com.api.mobigenz_be.repositories.CartRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CartItemServiceImp implements CartItemService {
 	@Autowired
 	private CartRepository cartRepository;
-
+	
 	@Autowired
 	private CartItemRepository cartItemRepository;
-
+	
 	private CartItemDTO cartItemMapToCartItemDto(CartItem cartItem) {
 		ProductDetailCartDto productDetailCartDto = this.cartRepository.getProductDetailByCartItemId(cartItem.getId());
 		return CartItemDTO
@@ -34,56 +28,53 @@ public class CartItemServiceImp implements CartItemService {
 				.productDetailCartDto(productDetailCartDto)
 				.build();
 	}
-
+	
 	private CartItem cartItemDtoMapToCartItem(CartItemDTO cartItemDTO) {
 		return CartItem
 				.builder()
 				.id(cartItemDTO.getId())
 				.amount(cartItemDTO.getAmount())
-				.productDetail(ProductDetail.builder().id(cartItemDTO.getProductDetailCartDto().getId()).build())
+				.productDetail(this.cartItemRepository.getProductDetailById(cartItemDTO.getProductDetailCartDto().getId()))
 				.build();
 	}
 
-    @Override
-    @Transactional
-    public CartItemDTO addCartItem(CartItemDTO cartItemDTO, Integer customerId) {
-        Cart cart = new Cart();
-        Optional<Cart> cartOptional = this.cartRepository.getCartByCustomerId(customerId);
-		CartItem cartItem = this.cartItemDtoMapToCartItem(cartItemDTO);
-		if(cartOptional.isPresent()) {
-        	Optional<CartItem> cartItemOptional = this.cartItemRepository.getCartItemByProductDetailIdAndCartId(
-					cartItem.getProductDetail().getId(),
-					cartOptional.get().getId()
-			);
-			if(cartItemOptional.isPresent()) {
-				cartItem = cartItemOptional.get();
-				int amount = cartItem.getAmount();
-				amount++;
-				cartItem.setAmount(amount);
-			}
-			cartItem.setCart(cartOptional.get());
-		} else {
-			cart = Cart.builder()
-						.customer(Customer.builder().id(customerId).build())
-						.itemsAmount(0)
-						.totalMoney(0.0)
-						.build();
-			this.cartRepository.save(cart);
-        	cartItem.setCart(cart);
-		}
-        cartItem = this.cartItemRepository.saveAndFlush(cartItem);
-        return this.cartItemMapToCartItemDto(cartItem);
-    }
+//	@Override
+//	public CartItemDTO createCartItem(CartItemDTO cartItemDTO) {
+//		CartItem cartItem = this.cartItemRepository.saveAndFlush(cartItemDtoMapToCartItem(cartItemDTO));
+//		return this.cartItemMapToCartItemDto(cartItem);
+//	}
 
 	@Override
-	public CartItemDTO updateCartItem(CartItemDTO cartItemDTO, Integer cart_id) {
-		CartItem cartItem = cartItemDtoMapToCartItem(cartItemDTO);
-		Cart cart = new Cart();
-		cart.setId(cart_id);
-		cartItem.setCart(cart);
-		cartItem = this.cartItemRepository.save(cartItem);
+	public CartItemDTO createCartItem(CartItemDTO cartItemDTO) {
+		CartItem cartItem = this.cartItemRepository.saveAndFlush(cartItemDtoMapToCartItem(cartItemDTO));
 		return this.cartItemMapToCartItemDto(cartItem);
 	}
+
+	@Override
+	public CartItemDTO updateCartItem(CartItemDTO cartItemDTO) {
+		CartItem cartItem = this.cartItemRepository.save(cartItemDtoMapToCartItem(cartItemDTO));
+		return this.cartItemMapToCartItemDto(cartItem);
+	}
+
+//	@Override
+//	public CartItemDTO addCartItem(CartItemDTO cartItemDTO, Integer customerId) {
+//		return null;
+//	}
+//
+//	@Override
+//	public CartItemDTO updateCartItem(CartItemDTO cartItemDTO, Integer cart_id) {
+//		return null;
+//	}
+
+//	@Override
+//	public CartItemDTO addCartItem(CartItemDTO cartItemDTO, Integer customerId) {
+//		return null;
+//	}
+//
+//	@Override
+//	public CartItemDTO updateCartItem(CartItemDTO cartItemDTO, Integer cart_id) {
+//		return null;
+//	}
 
 	@Override
 	public void deleteCartItem(Integer id) {
