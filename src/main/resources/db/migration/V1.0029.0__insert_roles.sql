f6ff449dcc778c3f096f2a07860594c0e46a16b9
LeVanTrangZ <levantrang4302@gmail.com>
2022-12-13 21:42:16 +0700

    insert data
    This reverts commit a96c8dbe

---
+++ src/main/resources/db/migration/V1.0029.0__insert_roles.sql
@@ -0,0 +1,3 @@
+insert into roles(role_name) values ("Admin")
+insert into roles(role_name) values ("Employee")
+insert into roles(role_name) values ("Customer")
---
+++ src/main/resources/db/migration/V1.0030.0__insert_account.sql
@@ -0,0 +1,2 @@
+insert into accounts(email,password,phoneNumber)
+values ("admin@gmail.com","$2a$10$ASLr8t9/14uH2mSuzhOIsuZ3iwHlrq3J51ZzTyZxVD243Qr1lbYIS","0967973101")
